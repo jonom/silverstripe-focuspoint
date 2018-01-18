5c08c0aee6a1d9ef386a2dace13f8294336cd63e
Roman Schmid <bummzack@gmail.com>
2018-01-18 23:24:47 +0100

    migration stub.

--- src/Dev/FocusPointMigrationTask.php
+++ src/Dev/FocusPointMigrationTask.php
@@ -3,7 +3,23 @@
 namespace JonoM\FocusPoint\Dev;
 
 
-class FocusPointMigrationTask extends \SilverStripe\Dev\MigrationTask
-{
+use SilverStripe\Dev\MigrationTask;
 
+class FocusPointMigrationTask extends MigrationTask
+{
+    // upgrade to new version
+    public function up()
+    {
+        //TODO: Check if FocusX,Y field still exists, if not, exit.
+        //TODO: Copy field values from FocusX,Y over to FocusPointFocusX,Y
+        //TODO: Delete FocusX,Y fields
+    }
+
+    // Revert to old version
+    public function down()
+    {
+        //TODO: Check if FocusPointFocusX,Y exists. If not, exit
+        //TODO: Copy field values from FocusPointFocusX,Y over to FocusX,Y
+        //TODO: Delete FocusPointFocusX,Y fields
+    }
 }
