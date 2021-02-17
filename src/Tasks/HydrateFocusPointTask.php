4e1219b1435ad78262eca432a79ae74d9a76aecd
Damian Mooyman <damian.mooyman@littlegiant.co.nz>
2021-02-17 17:17:58 +1300

    Add helpful task for hydrating width / height cache

---
+++ src/Tasks/HydrateFocusPointTask.php
@@ -0,0 +1,51 @@
+<?php
+
+use SilverStripe\Assets\Image;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\Dev\Debug;
+use SilverStripe\ORM\ValidationException;
+use SilverStripe\Versioned\ChangeSet;
+use SilverStripe\Versioned\Versioned;
+
+class HydrateFocusPointTask extends BuildTask
+{
+    private static $segment = 'HydrateFocusPointTask';
+
+    protected $title = 'Hydrate the focuspoint extension image size cache';
+
+    protected $description = 'Run this task to cache all image sizes, and speed up image generation';
+
+    /**
+     * @param HTTPRequest $request
+     * @throws ValidationException
+     */
+    public function run($request)
+    {
+        // Get all images missing a width / height
+        $images = Versioned::get_by_stage(Image::class, Versioned::DRAFT)->filterAny([
+            'FocusPointWidth'  => 0,
+            'FocusPointHeight' => 0,
+        ]);
+        Debug::message('Found ' . $images->count() . ' images to hydrate');
+
+        /** @var Image $image */
+        $changeset = ChangeSet::create();
+        $changeset->write();
+        foreach ($images as $image) {
+            // Skip images that aren't on the filesystem
+            if (!$image->exists()) {
+                continue;
+            }
+
+            // Save, and maybe publish
+            $image->write();
+            if ($image->isPublished()) {
+                $changeset->addObject($image);
+            }
+        }
+
+        // One changeset for all items to publish
+        $changeset->publish();
+    }
+}
