4b5421b7f3dff42bd26d951143455ab9ebb8a878
Damian Mooyman <damian.mooyman@littlegiant.co.nz>
2021-02-18 13:51:30 +1300

    Fix issue with DBFile not storing DBFocusPoint

--- src/Extensions/FocusPointDBFileExtension.php
+++ src/Extensions/FocusPointDBFileExtension.php
@@ -2,7 +2,33 @@
 
 namespace JonoM\FocusPoint\Extensions;
 
+use JonoM\FocusPoint\FieldType\DBFocusPoint;
+
 class FocusPointDBFileExtension extends FocusPointExtension
 {
+    /**
+     * Get focus point for this image; Prevent failover to backend Image
+     *
+     * @return DBFocusPoint
+     */
+    public function getFocusPoint(): ?DBFocusPoint
+    {
+        if (property_exists($this->owner, 'focuspoint_object')) {
+            return $this->owner->focuspoint_object;
+        }
 
+        return null;
+    }
+
+    /**
+     * Set a new focus point
+     *
+     * @param DBFocusPoint|null $point
+     * @return $this
+     */
+    public function setFocusPoint(?DBFocusPoint $point): self
+    {
+        $this->owner->focuspoint_object = $point;
+        return $this;
+    }
 }
